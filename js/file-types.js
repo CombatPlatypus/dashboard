7e3797ce54ff82dd567620600e58a7a28852cb13
CombatPlatypus <lukejunqueira@hotmail.com>
2026-07-28 15:44:43 -0300

    Add files via upload

---
+++ js/file-types.js
@@ -0,0 +1,39 @@
+/**
+ * Ícones locais utilizados como fallback
+ * para os tipos de arquivos do Google Drive.
+ */
+export const DRIVE_ITEM_ICONS = {
+
+    "application/pdf":
+        "images/files-types/pdf-icon.svg",
+
+    "application/zip":
+        "images/files-types/zip-icon.svg",
+
+    "application/vnd.google-apps.spreadsheet":
+        "images/files-types/sheets-icon.svg",
+
+    "application/vnd.google-apps.document":
+        "images/files-types/docs-icon.svg",
+
+    "application/vnd.google-apps.presentation":
+        "images/files-types/slides-icon.svg"
+
+};
+
+
+/**
+ * Caminhos dos ícones genéricos.
+ */
+export const DRIVE_DEFAULT_ICONS = {
+
+    folder:
+        "images/files-types/folder-icon.svg",
+
+    image:
+        "images/files-types/image-icon.svg",
+
+    file:
+        "images/files-types/file-icon.svg"
+
+};
