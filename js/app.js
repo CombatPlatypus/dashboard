e4ce1c9e3dbd767952aedca9e9f9ccce9ed3deb1
CombatPlatypus <lukejunqueira@hotmail.com>
2026-07-28 03:05:08 -0300

    Add OAuth token request on login button click

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
+// JS DO FOUNDATION
+
 $(document).foundation()
+
+// API
+
+import { tokenClient } from "./auth.js";
+
+const loginButton = document.getElementById("loginButton");
+
+loginButton.addEventListener("click", () => {
+
+    if (!tokenClient) {
+        console.error("O cliente OAuth ainda não foi inicializado.");
+        return;
+    }
+
+    tokenClient.requestAccessToken({
+        prompt: "consent"
+    });
+
+});
