09469210ace3b9082083522c4dd12cb38ab02208
CombatPlatypus <lukejunqueira@hotmail.com>
2026-07-28 22:17:24 -0300

    Refactor CONFIG comments in config.js
    Updated comments for clarity and consistency.

--- js/config.js
+++ js/config.js
@@ -1,27 +1,20 @@
-// Configurações centralizadas da integração
-// com os serviços do Google.
+// DEFINE AS CONFIGURAÇÕES GERAIS DA INTEGRAÇÃO COM O GOOGLE
+
 export const CONFIG = {
 
     google: {
 
         clientId:
-            "671842519306-an0e8ahhftppp3v0bshjdq5d384ab1b9.apps.googleusercontent.com",
+            "671842519306-an0e8ahhftppp3v0bshjdq5d384ab1b9.apps.googleusercontent.com", // ID DA API GERADO PELO GOOGLE
 
         folderId:
-            "1SVNfsh32gbuELXgC5tSaJuuBDk8iiojr",
+            "1SVNfsh32gbuELXgC5tSaJuuBDk8iiojr", // ID DA PASTA DO DRIVE A SER MOSTRADA
 
         scopes: [
-
             "openid",
-
             "email",
-
             "profile",
-
             "https://www.googleapis.com/auth/drive.readonly"
-
         ]
-
     }
-
 };
