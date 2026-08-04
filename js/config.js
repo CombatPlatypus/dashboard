3764d96ee896fefa705e00450715b00d5a8316a5
CombatPlatypus <lukejunqueira@hotmail.com>
2026-08-04 20:17:11 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,9 +7,6 @@ export const CONFIG = {
         clientId:
             "671842519306-an0e8ahhftppp3v0bshjdq5d384ab1b9.apps.googleusercontent.com", // ID DA API GERADO PELO GOOGLE
 
-        folderId:
-            "1xnyeHACpXe5r0g5cKixaHAFEflh6SWf0", // ID DA PASTA DO DRIVE A SER MOSTRADA
-
         sheetsApiKey:
             "AIzaSyCATqioFrLxHy6STfRks_WGqmg0ynXq5nY", // CHAVE DA API DO SHEETS
 
