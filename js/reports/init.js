c5206927225b3625781c0f41a5acad0e04133460
CombatPlatypus <lukejunqueira@hotmail.com>
2026-08-26 02:15:35 -0300

    Update init.js

--- js/reports/init.js
+++ js/reports/init.js
@@ -2,10 +2,30 @@ import {
     initializePlanningLhList,
 } from "./planning.js";
 
+import {
+    initializeLossesRateReport,
+} from "./losses-rate.js";
+
+import {
+    initializeLossesRateCharts,
+} from "./losses-rate-charts.js";
+
+import {
+    initializeLossesRateImport,
+} from "./losses-rate-import.js";
+
+import {
+    initializeLossesRateExport,
+} from "./losses-rate-export.js";
+
 /* INICIALIZA O PAINEL DE RELATÓRIOS */
 
 function initializeReportsPanel() {
     initializePlanningLhList();
+    initializeLossesRateReport();
+    initializeLossesRateCharts();
+    initializeLossesRateImport();
+    initializeLossesRateExport();
 }
 
 if (
